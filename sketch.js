53b8ac3024ce6071d0723bd6560dd2b9b93318c2
N.KV.S.S.Rakesh <70564530+Rakesh0730@users.noreply.github.com>
2021-05-28 14:31:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,216 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var PLAY = 1;
+var END = 0;
+var gameState = PLAY;
+
+var boulderImg1, boulderImg2, boulder1, boulder2;
+var treeImg1, treeImg2, tree1, tree2;
+var trafficConeImg, trafficCone;
+var manholeImg, manhole;
+var cr1i, cr1ii, cr2i, cr2ii, cr3i, cr3ii, playerI, playerII;
+var token, coin;
+var backgr, road;
+
+function preload()
+{
+
+	backgr = loadImage("assets/Road.png");
+
+	boulderImg1 = loadImage("assets/boulder-1.png");
+	boulderImg2 = loadImage("assets/boulder-2.jpg");
+
+	trafficConeImg = loadImage("assets/obstacle1.png");
+	manholeImg = loadImage("assets/obstacle2.png");
+
+	treeImg1 = loadImage("assets/cartoon-tree-1.png");
+	treeImg2 = loadImage("assets/cartoon-tree-2.png");
+
+	cr1i = loadAnimation("assets/mainPlayer1.png", "assets/mainPlayer2.png");
+	cr1ii = loadImage("assets/mainPlayer3.png");
+
+	cr2i = loadAnimation("assets/opponent4.png", "assets/opponent5.png");
+	cr2ii = loadImage("assets/opponent6.png");
+	
+	cr3i = loadAnimation("assets/opponent7.png", "assets/opponent8.png");
+	cr3ii = loadImage("assets/opponent9.png");
+
+	token = loadImage("assets/Gold-Coin-PNG.png");
+
+	pointGroup = new Group();
+	opponentGroup = new Group();
+	obstacleGroup = new Group();
+	treeGroup = new Group();
+
+}
+
+function setup()
+{
+	createCanvas(600, 1300);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	road = createSprite(0, 0, 600, 1280);
+	road.addImage(backgr);
+	road.velocityY = 1;
+
+	playerI = createSprite(290, 1030, 50, 50);
+	playerI.addAnimation("cycling", cr1i);
+	
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw()
+{
+  rectMode(CENTER);
+  background(0);
+
+  drawSprites();
+
+  if(gameState === PLAY)
+  {
+
+	if(backgr.y > 1230)
+    {
+
+     backgr.y = 1030;
+	 
+    }
+
+	if(keyDown("left_arrow"))
+    {
+
+     playerI.x = playerI.x - 3;
+     
+    }
+    
+   if(keyDown("right_arrow"))
+    {
+	   
+     playerI.x = playerI.x + 3;  
+     
+    }
+
+	if(coinGroup.isTouching(playerI))
+    {
+
+      coinGroup.destroyEach();
+      score = score + 1;
+
+    }
+
+	if(obstacleGroup.isTouching(playerI)||opponentGroup.isTouching(playerI)||treeGroup.isTouching(playerI))
+    {
+
+      gameState = END;
+	  road.velocityY = 0;
+	  playerI.changeAnimation(playerII);
+
+    }
+
+  }
+  else if(gameState === END)
+  {
+
+	road.velocityY = 0;
+	playerI.changeAnimation(playerII);
+
+
+  }
+
+  spawnOpponents();
+  spawnPoints();
+  spawnObstacles();
+  spawnTrees();
+
+}
+
+function spawnOpponents()
+{
+	if(World.frameCount % 200 === 0)
+	{
+
+	  opponent = createSprite(200, 1280, 20, 20);
+	  opponent.scale = 0.2;
+
+	   r = Math.round(random(1, 2));
+	  if (r == 1)
+	  {
+	   opponent.addAnimation("cycling", cr2i);
+	   opponent.addImage(cr2ii);
+	  } 
+	   else
+	  {
+	   opponent.addAnimation("cycling", cr3i);
+	   opponent.addImage("cycling", cr3ii);
+	  } 
+	  
+	  opponent.x = Math.round(random(200, 400));
+	  
+	  position = Math.round;
+	 
+	  opponent.velocityY = (7 + (score / 4));
+	  opponent.setLifetime = 100;
+	  
+	  opponentGroup.add(opponent);
+	}
+}
+
+function spawnPoints(){
+
+	if(frameCount % 80 === 0)
+	{
+	
+	 var coin = createSprite(200, 1280, 20, 20);
+	 coin.addImage(token);
+	 coin.y =  random (200, 400);
+	 coin.scale = 0.05;
+	 scoin.velocityY = 4;
+	
+	 coin.lifetime = 300;
+	 player.depth = coin.depth + 1;
+	 pointGroup.add(coin);
+	
+	}
+
+}
+
+function spawnObstacles()
+{
+	if(World.frameCount % 300 === 0)
+	{
+
+	  obstacle = createSprite(200, 1280, 20, 20);
+	  opponent.scale = 0.2;
+
+	   r = Math.round(random(1, 2));
+	  if (r == 1)
+	  {
+	   opponent.addAnimation("cycling", cr2i);
+	   opponent.addImage(cr2ii);
+	  } 
+	   else
+	  {
+	   opponent.addAnimation("cycling", cr3i);
+	   opponent.addImage("cycling", cr3ii);
+	  } 
+	  
+	  opponent.x = Math.round(random(200, 400));
+	  
+	  position = Math.round;
+	 
+	  opponent.velocityY = (7 + (score / 4));
+	  opponent.setLifetime = 100;
+	  
+	  opponentGroup.add(opponent);
+	}
+}
