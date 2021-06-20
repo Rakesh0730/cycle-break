0ced6583c10f062f86ed4e9e3e4620395a359a2d
N.KV.S.S.Rakesh <70564530+Rakesh0730@users.noreply.github.com>
2021-06-20 17:09:17 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,64 +8,65 @@ var PLAY = 1;
 var END = 0;
 var gameState = PLAY;
 
-var boulderImg1, boulderImg2, boulder1, boulder2;
-var treeImg1, treeImg2, tree1, tree2;
-var trafficConeImg, trafficCone;
-var manholeImg, manhole;
+var boulderImg1, boulderImg2;
+var treeImg1, treeImg2;
+var trafficConeImg, manholeImg;
 var cr1i, cr1ii, cr2i, cr2ii, cr3i, cr3ii, playerI, playerII;
-var token, coin;
-var gameOverScreen, gameOverScreen;
+var token;
+var gameOverScreen;
 var backgr, road;
+var score = 0;
 
 function preload()
 {
 
-	backgr = loadImage("Road.png");
+	backgr = loadImage("assets/Road.png");
 
-	boulderImg1 = loadImage("boulder-1.png");
-	boulderImg2 = loadImage("boulder-2.jpg");
+	boulderImg1 = loadImage("assets/boulder-1.png");
+	boulderImg2 = loadImage("assets/boulder-2.jpg");
 
-	trafficConeImg = loadImage("obstacle1.png");
-	manholeImg = loadImage("obstacle2.png");
+	trafficConeImg = loadImage("assets/obstacle1.png");
+	manholeImg = loadImage("assets/obstacle2.png");
 
-	treeImg1 = loadImage("cartoon-tree-1.png");
-	treeImg2 = loadImage("cartoon-tree-2.png");
+	treeImg1 = loadImage("assets/cartoon-tree-1.png");
+	treeImg2 = loadImage("assets/cartoon-tree-2.png");
 
-	cr1i = loadAnimation("mainPlayer1.png", "mainPlayer2.png");
-	cr1ii = loadImage("mainPlayer3.png");
+	cr1i = loadAnimation("assets/mainPlayer1.png", "assets/mainPlayer2.png");
+	cr1ii = loadImage("assets/mainPlayer3.png");
 
-	cr2i = loadAnimation("opponent4.png", "opponent5.png");
-	cr2ii = loadImage("opponent6.png");
+	cr2i = loadAnimation("assets/opponent4.png", "assets/opponent5.png");
+	cr2ii = loadImage("assets/opponent6.png");
 	
-	cr3i = loadAnimation("opponent7.png", "opponent8.png");
-	cr3ii = loadImage("opponent9.png");
+	cr3i = loadAnimation("assets/opponent7.png", "assets/opponent8.png");
+	cr3ii = loadImage("assets/opponent9.png");
 	
-	gameOverScreen = loadImage("gameOver.png");
+	gameOverScreen = loadImage("assets/gameOver.png");
 
-	token = loadImage("Gold-Coin-PNG.png");
+	token = loadImage("assets/Gold-Coin-PNG.png");
 
-	pointGroup = new Group();
+	coinGroup = new Group();
 	opponentGroup = new Group();
 	obstacleGroup = new Group();
-	treeGroup = new Group();
+	tree1Group = new Group();
+	tree2Group = new Group();
 
 }
 
 function setup()
 {
-	createCanvas(1500, 1500);
+	createCanvas(1520, 750);
 
 	engine = Engine.create();
 	world = engine.world;
 
-	road = createSprite(0, 0, 1280, 600);
+	road = createSprite(100, 80);
 	road.addImage(backgr);
-	road.velocityY = 1;
-	road.scale = 0.10;
+	road.velocityX = -1;
+	road.scale = 0.8;
 
-	playerI = createSprite(290, 1030, 50, 50);
+	playerI = createSprite(100, 640, 50, 50);
 	playerI.addAnimation("cycling", cr1i);
-	playerI.scale = 1;
+	playerI.scale = 0.1;
 
 
 	Engine.run(engine);
@@ -83,24 +84,24 @@ function draw()
   if(gameState === PLAY)
   {
 
-	if(road.y > 1230)
+	if(road.x < 100)
     {
 
-     road.y = 1030;
+     road.x = width/2;
 	 
     }
 
-	if(keyDown("left_arrow"))
+	if(keyDown("up_arrow"))
     {
 
-     playerI.x = playerI.x - 3;
+     playerI.y = playerI.y - 5;
      
     }
     
-   if(keyDown("right_arrow"))
+   if(keyDown("down_arrow"))
     {
 	   
-     playerI.x = playerI.x + 3;  
+     playerI.y = playerI.y + 5;  
      
     }
 
@@ -112,27 +113,41 @@ function draw()
 
     }
 
-	if(obstacleGroup.isTouching(playerI)||opponentGroup.isTouching(playerI)||treeGroup.isTouching(playerI))
+	if(obstacleGroup.isTouching(playerI)||opponentGroup.isTouching(playerI)||tree1Group.isTouching(playerI)||tree2Group.isTouching(playerI))
     {
 
-          gameState = END;
+      gameState = END;
 	  road.velocityY = 0;
 	  playerI.changeAnimation(playerII);
 
     }
 
   }
+
   else if(gameState === END)
   {
 
 	road.velocityY = 0;
-	player.x = 300;
-	playerI.changeAnimation(playerII);
 	gameOver.addImage(gameOverScreen);
-	cr2i.changeAnimation(cr2ii);
-	
 
+	playerI.changeAnimation(playerII);
+	cr2i.changeAnimation(cr2ii);
+	cr3i.changeAnimation(cr3ii);
+
+	playerI.velocityY = 0
+	cr2i.velocityY = 0
+	cr3i.velocityY = 0
+
+	coinGroup.destroyEach();
+	opponentGroup.destroyEach();
+	obstacleGroup.destroyEach();
+	tree1Group.destroyEach();
+	tree2Group.destroyEach();
   }
+
+  textSize(20);
+  text("score: " + score, 1400, 700);
+  
 
   spawnOpponents();
   spawnPoints();
@@ -143,10 +158,10 @@ function draw()
 
 function spawnOpponents()
 {
-	if(World.frameCount % 200 === 0)
+	if(World.frameCount % 100 === 0)
 	{
 
-	  opponent = createSprite(200, 1280, 20, 20);
+	  opponent = createSprite(300, 0, 20, 20);
 	  opponent.scale = 0.2;
 
 	   r = Math.round(random(1, 2));
@@ -166,7 +181,7 @@ function spawnOpponents()
 	  position = Math.round;
 	 
 	  opponent.velocityY = (7 + (score / 4));
-	  opponent.setLifetime = 100;
+	  opponent.setLifetime = 180;
 	  
 	  opponentGroup.add(opponent);
 	}
@@ -177,15 +192,15 @@ function spawnPoints(){
 	if(frameCount % 80 === 0)
 	{
 	
-	 var coin = createSprite(200, 1280, 20, 20);
+	 var coin = createSprite(1200, 600, 20, 20);
 	 coin.addImage(token);
-	 coin.y =  random (200, 400);
+	 coin.y =  random(200, 800);
 	 coin.scale = 0.05;
-	 scoin.velocityY = 4;
+	 coin.velocityX = -4;
 	
 	 coin.lifetime = 300;
-	 player.depth = coin.depth + 1;
-	 pointGroup.add(coin);
+	 playerI.depth = coin.depth + 1;
+	 coinGroup.add(coin);
 	
 	}
 
@@ -193,31 +208,69 @@ function spawnPoints(){
 
 function spawnObstacles()
 {
-	if(World.frameCount % 300 === 0)
+	if(World.frameCount % 60 === 0)
 	{
 
-	  obstacle = createSprite(200, 1280, 20, 20);
-	  opponent.scale = 0.2;
+	  obstacle = createSprite(200, 0, 20, 20);
+	  obstacle.scale = 0.2;
+	  obstacle.velocityX = 3;
+	   a = Math.round(random(1, 2, 3, 4));
+	  if (a == 1)
+	  {
+	   obstacle.addImage(boulderImg1);
+	  } 
+	   else if (a == 2)
+	  {
+	   obstacle.addImage(boulderImg2);
+	  } 
+	  	else if (a == 3)
+	  {
+		obstacle.addImage(trafficConeImg);
+	  }
+	  	else 
+	  {
+		obstacle.addImage(manholeImg);
+	  }
 
-	   r = Math.round(random(1, 2));
-	  if (r == 1)
-	  {
-	   opponent.addAnimation("cycling", cr2i);
-	   opponent.addImage(cr2ii);
-	  } 
-	   else
-	  {
-	   opponent.addAnimation("cycling", cr3i);
-	   opponent.addImage(cr3ii);
-	  } 
-	  
-	  opponent.x = Math.round(random(200, 400));
+	  obstacle.x = Math.round(random(100, 1200));
 	  
 	  position = Math.round;
 	 
-	  opponent.velocityY = (7 + (score / 4));
-	  opponent.setLifetime = 100;
+	  obstacle.velocityY = (7 + (score / 4));
+	  obstacle.setLifetime = 180;
 	  
-	  opponentGroup.add(opponent);
+	  obstacleGroup.add(obstacle);
 	}
+}
+
+function spawnTrees()
+{
+	if(World.frameCount % 20 === 0)
+	{
+
+	  var tree1 = createSprite(1520, 200, 20, 20);
+	  tree1.addImage(treeImg1);
+
+	  tree1.y = 200;
+	 
+	  tree1.velocityX = -5;
+	  tree1.setLifetime = 304;
+	  
+	  tree1Group.add(tree1);
+	}
+
+	if(World.frameCount % 20 === 0)
+	{
+
+	  var tree2 = createSprite(1520, 550, 20, 20);
+	  tree2.addImage(treeImg1);
+
+	  tree2.y = 200;
+	 
+	  tree2.velocityX = -5;
+	  tree2.setLifetime = 304;
+	  
+	  tree1Group.add(tree2);
+	}
+
 }
